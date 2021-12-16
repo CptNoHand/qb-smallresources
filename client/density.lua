ca7cbc88b5b3baf4ef69da1ec531080d4be5f623
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-16 14:32:36 +0100

    Update density.lua

--- client/density.lua
+++ client/density.lua
@@ -1,6 +1,6 @@
 local density = 1.0
-local parkeddensity = 0.2
-local cardensity = 0.2
+local parkeddensity = 0.1
+local cardensity = 0.1
 CreateThread(function()
 	while true do
 		SetParkedVehicleDensityMultiplierThisFrame(parkeddensity)
