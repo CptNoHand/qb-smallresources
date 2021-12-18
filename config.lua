b6f4a979382c810fe176292cb4471c120425e975
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-18 17:06:46 +0100

    added items

--- config.lua
+++ config.lua
@@ -22,6 +22,16 @@ ConsumeablesEat = {
     ["burger-meatfree"] = math.random(35, 54),
     ["burger-fries"] = math.random(35, 54),
     ["crisps"] = math.random(10, 20),
+    ["sushirolls"] = math.random(20, 32), -- 1
+    ["pizzaslice"] = math.random(12, 16), -- 2
+    ["cookie"] = math.random(5, 10), -- 3
+    ["muffin"] = math.random(5, 10), -- 4
+    ["fries"] = math.random(10, 18), -- 5
+    ["donut"] = math.random(8, 26), -- 6
+    ["icecream"] = math.random(5, 12), -- 7
+    ["cupcake"] = math.random(7, 18), -- 8
+    ["gum"] = math.random(3, 8), -- 9
+    ["ramen"] = math.random(3, 15), -- 10
 }
 
 ConsumeablesDrink = {
@@ -30,6 +40,12 @@ ConsumeablesDrink = {
     ["coffee"] = math.random(40, 50),
     ["burger-softdrink"] = math.random(40, 50),
     ["burger-mshake"] = math.random(40, 50),
+    ["sprite"] = math.random(35, 54), -- 1
+    ["pepsi"] = math.random(35, 54), -- 2
+    ["mtndew"] = math.random(35, 54), -- 3
+    ["lemonade"] = math.random(35, 54), -- 4
+    ["icedtea"] = math.random(35, 54), -- 5
+    ["milk"] = math.random(35, 54),
 }
 
 ConsumeablesAlcohol = {
